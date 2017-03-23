58903a7a97347742dec1bcd209d5f852f07b5001
DMon18 <101190737@student.swin.edu.au>
2017-03-23 21:13:42 +1100

    Change the color and position of the score

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -44,9 +44,9 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont",0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont",0, 50);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.Red, 0, 30);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.Black, "GameFont",0, 50);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.Black, "GameFont",0, 80);
 
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 150);
 			}
